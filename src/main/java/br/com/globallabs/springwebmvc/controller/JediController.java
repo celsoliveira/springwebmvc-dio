d71548eced9bb2649a5201eddc109d6b511e7c79
Celso de Oliveira Silva <celso_os@yahoo.com.br>
2021-06-11 21:03:25 -0300

    First committed from project

---
+++ src/main/java/br/com/globallabs/springwebmvc/controller/JediController.java
@@ -0,0 +1,50 @@
+package br.com.globallabs.springwebmvc.controller;
+
+import br.com.globallabs.springwebmvc.model.Jedi;
+import br.com.globallabs.springwebmvc.repository.JediRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+import javax.validation.Valid;
+
+@Controller
+public class JediController {
+
+    @Autowired
+    private JediRepository repository;
+
+    @GetMapping("/jedi")
+    public ModelAndView jedi(){
+        final ModelAndView modelAndView = new ModelAndView();
+        modelAndView.setViewName("jedi");
+
+        modelAndView.addObject("allJedi", repository.getAllJedi());
+
+        return modelAndView;
+    }
+
+    @GetMapping("/new-jedi")
+    public ModelAndView newJedi(){
+        final ModelAndView modelAndView = new ModelAndView();
+        modelAndView.addObject("jedi", new Jedi() );
+        modelAndView.setViewName("new-jedi");
+        return modelAndView;
+    }
+
+    @PostMapping("/jedi")
+    public String createJedi(@Valid @ModelAttribute Jedi jedi, BindingResult result, RedirectAttributes redirect){
+        if (result.hasErrors()){
+                return "new-jedi";
+        };
+        repository.add(jedi);
+        redirect.addFlashAttribute("message", "Jedi cadastrado com sucesso");
+        return "redirect:jedi";
+    }
+
+
+}
